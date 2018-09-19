66f9ce4aee3f841cd842a72868ecb8c1a78834d8
Peter Ohmann <ohmann@cs.wisc.edu>
2018-09-19 09:50:25 -0500

    Use a nicer graphics window for the test program
    - built on ACM's GraphicsProgram

--- BucketTester.java
+++ BucketTester.java
@@ -1,10 +1,24 @@
-import acm.program.CommandLineProgram;
+import acm.program.GraphicsProgram;
+import acm.graphics.GLabel;
 
-public class BucketTester extends CommandLineProgram {
+public class BucketTester extends GraphicsProgram {
+    private GLabel averageLabel;
+    private GLabel bucketCountLabel;
+
+    public void init() {
+        averageLabel = new GLabel("The average is: ?");
+        bucketCountLabel = new GLabel("There are 0 numbers in the bucket");
+
+        this.add(averageLabel, 100, 100);
+        this.add(bucketCountLabel, 100, 200);
+    }
+
+
     public void run() {
         NumberBucket myBucket = new NumberBucket();
         myBucket.addNumber(3);
         myBucket.addNumber(4);
-        System.out.println(myBucket.average());
+        averageLabel.setLabel("The average is: " + myBucket.average());
+        bucketCountLabel.setLabel("There are 2 numbers in the bucket");
     }
 }
