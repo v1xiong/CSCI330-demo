3db89157f8e1af3f582a9a85868059f66ec777a2
Peter Ohmann <ohmann@cs.wisc.edu>
2018-09-19 09:54:46 -0500

    Allow the user to enter numbers for testing
    - no longer hard-coded!

--- BucketTester.java
+++ BucketTester.java
@@ -3,8 +3,14 @@ import acm.program.CommandLineProgram;
 public class BucketTester extends CommandLineProgram {
     public void run() {
         NumberBucket myBucket = new NumberBucket();
-        myBucket.addNumber(3);
-        myBucket.addNumber(4);
-        System.out.println(myBucket.average());
+
+        int userEntry;
+        while (true) {
+            userEntry = this.readInt("Enter an int (-1 to stop)");
+            if (userEntry == -1)
+                break;
+            myBucket.addNumber(userEntry);
+        }
+        System.out.println("The average is: " + myBucket.average());
     }
 }
